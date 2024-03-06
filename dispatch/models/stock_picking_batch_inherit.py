f9c14cf49ade477fd3ce5098b48980d3b97e3df7
Hritik Kumar <hrku@odoo.com>
2024-03-06 18:43:32 +0530

    added thumbnail in gantt view, made label for progressbar

--- dispatch/models/stock_picking_batch_inherit.py
+++ dispatch/models/stock_picking_batch_inherit.py
@@ -5,17 +5,18 @@ from dateutil.relativedelta import relativedelta
 class InheritInventory(models.Model):
     _inherit = 'stock.picking.batch'
 
-    dock = fields.Many2one('model.dock',string="Dock")
+    dock_id = fields.Many2one('model.dock',string="Dock")
 
-    vehicle = fields.Many2one("fleet.vehicle",string="Vehicle")
+    vehicle_id = fields.Many2one("fleet.vehicle",string="Vehicle")
 
-    category = fields.Many2one("fleet.vehicle.model.category",string="Vehicle Category")
-    partner_ids = fields.Many2one('res.partner')
-    driver_id = fields.Many2one("fleet.vehicle.driver_id", string="Icon")
-    # partner_image = fields.Binary(related='partner_ids.driver_id')
+    category_id = fields.Many2one("fleet.vehicle.model.category",string="Vehicle Category")
+    partner_id = fields.Many2one('res.partner')
+
+    total_weight = fields.Float(string="Weight",readonly=True)
+    total_volume = fields.Float(string="Volume", readonly=True)
     
-    weight = fields.Float(compute="_compute_weight", string="Weight", store=True)
-    volume = fields.Float(compute="_compute_volume", string="Volume", store=True)
+    weight = fields.Float(compute="_compute_weight", store=True)
+    volume = fields.Float(compute="_compute_volume", store=True)
     transfers = fields.Integer(compute="_compute_transfers", string="Transfers", store="True")
     lines = fields.Integer(compute="_compute_lines", string="Lines", store="True")
 
@@ -25,11 +26,10 @@ class InheritInventory(models.Model):
             current_weight = 0
             for move_id in record.move_ids:
                 current_weight = current_weight + move_id.product_qty*move_id.product_id.weight
+                record.total_weight = current_weight
 
-            if record.category.max_weight >0:
-                record.weight = (current_weight / record.category.max_weight)*100
-            else:
-                record.weight = 1
+            if record.category_id.max_weight >0:
+                record.weight = (current_weight / record.category_id.max_weight)*100
             
             if record.weight>100:
                 record.weight = 100
@@ -41,11 +41,10 @@ class InheritInventory(models.Model):
             current_volume = 0
             for move_id in record.move_ids:
                 current_volume = current_volume + move_id.product_qty*move_id.product_id.volume
+                record.total_volume = current_volume
 
-            if record.category.max_volume >0:
-                record.volume = (current_volume / record.category.max_volume)*100
-            else:
-                record.volume = 1
+            if record.category_id.max_volume >0:
+                record.volume = (current_volume / record.category_id.max_volume)*100
             
             if record.volume>100:
                 record.volume = 100
@@ -62,3 +61,7 @@ class InheritInventory(models.Model):
             curr = len(record.move_line_ids)
             record.lines = curr
 
+    @api.depends('weight','volume')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = record.name + ":" + str(round(record.weight)) + " kg / " + str(round(record.volume)) + " m^3"
